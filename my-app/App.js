bbad7a3a28a86a50f8bfc805d864b358af289ed4
joel-2004 <jrjoel484@gmail.com>
2024-01-26 06:15:03 +0530

    Init

---
+++ my-app/App.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+import Form from "./Components/Form";
+
+export default function App() {
+  return (
+    <Form></Form>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
+
