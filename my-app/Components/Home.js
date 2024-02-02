db150d30c689f834b78fc6bea212c2646b9b9a7f
joel-2004 <jrjoel484@gmail.com>
2024-02-02 15:02:09 +0530

    form for income

--- my-app/Components/Home.js
+++ my-app/Components/Home.js
@@ -1,20 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, Pressable, StyleSheet, Text, TextInput, TextInputComponent, View } from 'react-native'
+import React, { useState } from 'react'
 
 export default function Home() {
     let temp = 9999999;
+    const handleIncomePress = () => {
+        console.log(income)
+        //setIncome(0);
+    }
+    const [income, setIncome] = useState(0);
     return (
         <View>
+            {
+                //Begining of main View
+            }
+            {
+                //TopHeader
+            }
             <View style={styles.topContainer}>
                 <Text>Expense Tracker</Text>
                 <View style={styles.incomeBalanceRow}>
-                    <Text style={styles.incomeBalanceText}>Income: {temp}</Text>
+                    <Text style={styles.incomeBalanceText}>Income: {income}</Text>
                     <Text style={styles.incomeBalanceText}>Balance: {temp}</Text>
                 </View>
+                <View style={styles.incomeBalanceRow}>
+                    <TextInput style={styles.incomeInput} inputMode='numeric' defaultValue={income}
+                        onChangeText={(e) => setIncome(e)}
+                    ></TextInput>
+                    <Pressable onPress={handleIncomePress}>
+                        Set Income
+                    </Pressable>
+                </View>
             </View>
+
+            {
+                //Transfer History
+            }
             <View style={styles.transferContainer}>
                 <Text>Transfer History</Text>
             </View>
+            {
+                //End of main View
+            }
         </View>
     )
 }
@@ -39,6 +65,9 @@ const styles = StyleSheet.create({
     }
     ,
     incomeBalanceRow: {
-        flexDirection: "row"
+        flexDirection: "row",
+    },
+    incomeInput: {
+        backgroundColor: "blue",
     }
 })
