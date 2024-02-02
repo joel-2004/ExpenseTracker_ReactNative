59243b3e26216e3aa284154d036bfe3151fbf3aa
joel-2004 <jrjoel484@gmail.com>
2024-02-02 21:07:56 +0530

    Form to add amount and purpose

--- my-app/Components/Home.js
+++ my-app/Components/Home.js
@@ -2,46 +2,82 @@ import { Button, Pressable, StyleSheet, Text, TextInput, TextInputComponent, Vie
 import React, { useState } from 'react'
 
 export default function Home() {
-    let temp = 9999999;
+    let temp = 0;
+    const [income, setIncome] = useState(0);
+    const [balance, setBalance] = useState(0);
+    const [transferHistory, setTransferHistory] = useState([]);
+    const [amount, setAmount] = useState(0);
+    const [purpose, setPurpose] = useState("");
     const handleIncomePress = () => {
         console.log(income)
         //setIncome(0);
+        setBalance(income);
     }
-    const [income, setIncome] = useState(0);
+
+    const handleAddExpense = () => {
+        const newData = {
+            amount: amount,
+            purpose: purpose
+        }
+        setTransferHistory([...transferHistory, newData]);
+        setAmount(0);
+        setPurpose("");
+        console.log(transferHistory);
+    }
     return (
-        <View>
-            {
-                //Begining of main View
-            }
-            {
-                //TopHeader
-            }
-            <View style={styles.topContainer}>
-                <Text>Expense Tracker</Text>
-                <View style={styles.incomeBalanceRow}>
-                    <Text style={styles.incomeBalanceText}>Income: {income}</Text>
-                    <Text style={styles.incomeBalanceText}>Balance: {temp}</Text>
+        <>
+            <View>
+                {
+                    //Begining of main View
+                }
+                {
+                    //TopHeader
+                }
+                <View style={styles.topContainer}>
+                    <Text>Expense Tracker</Text>
+                    <View style={styles.incomeBalanceRow}>
+                        <Text style={styles.incomeBalanceText}>Income: {income}</Text>
+                        <Text style={styles.incomeBalanceText}>Balance: {balance}</Text>
+                    </View>
+                    <View style={styles.incomeBalanceRow}>
+                        <TextInput style={styles.incomeInput} inputMode='numeric' value={income}
+                            onChangeText={(e) => setIncome(e)}
+                        ></TextInput>
+                        <Pressable style={styles.setIncomeButton} onPress={handleIncomePress}>
+                            <Text> Set Balance</Text>
+                        </Pressable>
+                    </View>
                 </View>
-                <View style={styles.incomeBalanceRow}>
-                    <TextInput style={styles.incomeInput} inputMode='numeric' defaultValue={income}
-                        onChangeText={(e) => setIncome(e)}
-                    ></TextInput>
-                    <Pressable onPress={handleIncomePress}>
-                        Set Income
+
+                {
+                    //Transfer History
+                }
+                <View style={styles.transferContainer}>
+                    <Text>Transfer History</Text>
+                </View>
+
+                {
+                    //Form for transfer history
+                }
+                <View>
+                    <View style={styles.incomeBalanceRow}>
+                        <Text >Amount</Text>
+                        <TextInput style={styles.incomeInput} value={amount} onChangeText={(e) => setAmount(e)} inputMode='numeric'></TextInput>
+                    </View>
+                    <View style={styles.incomeBalanceRow}>
+                        <Text >Purpose</Text>
+                        <TextInput style={styles.incomeInput} value={purpose} onChangeText={(e) => setPurpose(e)} inputMode='text'></TextInput>
+                    </View>
+                    <Pressable style={styles.setIncomeButton} onPress={handleAddExpense}>
+                        <Text>Add</Text>
                     </Pressable>
                 </View>
-            </View>
+                {
+                    //End of main View
+                }
 
-            {
-                //Transfer History
-            }
-            <View style={styles.transferContainer}>
-                <Text>Transfer History</Text>
             </View>
-            {
-                //End of main View
-            }
-        </View>
+        </>
     )
 }
 
@@ -69,5 +105,11 @@ const styles = StyleSheet.create({
     },
     incomeInput: {
         backgroundColor: "blue",
+        margin: 5
+    },
+    setIncomeButton: {
+        backfaceVisibility: "visible",
+        paddingLeft: 4,
+        backgroundColor: "green"
     }
 })
